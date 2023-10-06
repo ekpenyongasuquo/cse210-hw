bb6bd63c3c16dc6cf872a88d851a80e3984cbb41
ekpenyongasuquo <eamfon@outlook.com>
2023-10-06 15:25:45 +0100

    Make correction

--- prove/Develop02/PromptGenerator.cs
+++ prove/Develop02/PromptGenerator.cs
@@ -7,8 +7,8 @@ public class PromptGenerator
     {
         _prompts = new List<string>()
         {
-        "Who was the most knowledgable person I interacted with today?",
-        "What was the interesting part of my day?",
+        "Who was the most interesting person I interacted with today?",
+        "What was the best part of my day?",
         "How did I see the hand of the Lord in my life today?",
         "What was the strongest emotion I felt today?",
         "If I had one thing I could do over today, what would it be?"
